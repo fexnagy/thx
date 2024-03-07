a793c4ad904de1bf9da2f1050f41c7116d8b1eca
Felix Nagy <fnagy@st.noorderpoort.nl>
2024-03-07 11:05:40 +0100

    init (with dashboard)

---
+++ base/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+
+def settings(request):
+    return render(request, "settings.html")
